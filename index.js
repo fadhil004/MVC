31cfd14f62074e14e977aad361a9d4a161701f55
fadhil004 <m.fadhil238@gmail.com>
2024-04-07 22:04:54 +0700

    processs.argv

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const fs = require('fs')
+const process = require('process')
+const input = process.argv
+
+function penjumlahan(angka1, angka2) {
+    return angka1 + angka2
+}
+
+const hasil = penjumlahan(+input[2],+input[3])
+console.log('Hasil Penjumlahan', hasil)
